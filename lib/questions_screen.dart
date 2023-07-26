1ec7a4bda114a0044ff7a3ed56b3fb5b5aa1c45f
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-07-26 22:57:31 +0530

    showing options using map

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,32 +13,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer Text...',
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer Text...',
-          ),
-          AnswerButton(
-            onTap: () {},
-            answerText: 'Answer Text...',
-          ),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(
+              answerText: item,
+              onTap: () {},
+            );
+          }),
         ],
       ),
     );
