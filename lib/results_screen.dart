83ab54686bbaef7a4628ba9adf40d1ea049c58e4
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-07-27 22:01:38 +0530

    creating a summary data to check correct asnwers

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,8 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
+
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (int i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(context) {
     return SizedBox(
